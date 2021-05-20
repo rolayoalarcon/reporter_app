e3d83eb73bbc58d54e38f565bcb1b89d2d23453a
rolayoalarcon <rolayoalarcon@gmail.com>
2021-05-20 15:02:21 +0200

    added info

--- app.R
+++ app.R
@@ -13,15 +13,20 @@ ui <- fluidPage(title = "Choosing gene expression",
                              sidebarPanel(
                                selectInput(inputId = "c_method_cds",
                                            label = "Clustering Method:",
-                                           choices = list("minmax", "complete")),
+                                           choices = list("minmax", "complete", "ward.D2", "median", "centroid")),
                                selectInput(inputId = "d_metric_cds",
                                            label = "Distance Metric:",
-                                           choices = list("cosine", "pearson")),
+                                           choices = list("cosine", "spearman", "pearson")),
                                numericInput(inputId = "k_clusters_cds",
                                             label = "Number of clusters:",
                                             value = 20, min=1),
                                actionButton(inputId = "cluster_param_cds", 
-                                            label = "Submit", icon = NULL, width = NULL)),
+                                            label = "Submit", icon = NULL, width = NULL),
+                               numericInput(inputId = "high_cds",
+                                            label="Highlight Cluster:",
+                                            value=0, min = 0),
+                               actionButton(inputId = "high_param_cds",
+                                            label = "Highlight")),
                              mainPanel(plotOutput("dendo_cds"))
                            ),
                            sidebarLayout(
@@ -59,15 +64,20 @@ ui <- fluidPage(title = "Choosing gene expression",
                              sidebarPanel(
                                selectInput(inputId = "c_method_srna",
                                            label = "Clustering Method:",
-                                           choices = list("minmax", "complete")),
+                                           choices = list("minmax", "complete", "ward.D2", "median", "centroid")),
                                selectInput(inputId = "d_metric_srna",
                                            label = "Distance Metric:",
-                                           choices = list("cosine", "pearson")),
+                                           choices = list("cosine", "spearman","pearson")),
                                numericInput(inputId = "k_clusters_srna",
                                             label = "Number of clusters:",
                                             value = 20, min=1),
                                actionButton(inputId = "cluster_param_srna", 
-                                            label = "Submit", icon = NULL, width = NULL)),
+                                            label = "Submit", icon = NULL, width = NULL),
+                               numericInput(inputId = "high_srna",
+                                            label="Highlight Cluster:",
+                                            value=0, min = 0),
+                               actionButton(inputId = "high_param_srna",
+                                            label = "Highlight")),
                              mainPanel(plotOutput("dendo_srna"))
                            ),
                            sidebarLayout(
@@ -109,7 +119,23 @@ cds_rlog.fc <- read_tsv("shiny_data/cds_logFC.tsv") %>%
 srna_rlog.fc <- read_tsv("shiny_data/srna_logFC.tsv") %>% 
   column_to_rownames("gene_name")
 
+# Some average expression info
+joint_cds.avg <- read_tsv("shiny_data/joint_cds_avg.tsv") %>% 
+  column_to_rownames("gene_name")
 
+cds_rowmeans <- rowMeans(joint_cds.avg)
+cds_avgExp <- data.frame("gene_name"=names(cds_rowmeans),
+                         "Average_Expression"=cds_rowmeans)
+
+
+joint_srna.avg <- read_tsv("shiny_data/joint_srna_avg.tsv") %>% 
+  column_to_rownames("gene_name")
+
+srna_rowmeans <- rowMeans(joint_srna.avg)
+srna_avgExp <- data.frame("gene_name"=names(srna_rowmeans),
+                         "Average_Expression"=srna_rowmeans)
+
+# Genomic features
 salm_features <- read_tsv("shiny_data/merged_features_salmonella.tsv") %>% 
   mutate(organism="Salmonella",
          gene_name=paste0(locus_tag, "-Salmonella")) %>% 
@@ -130,21 +156,82 @@ sussane.df <- read_tsv("shiny_data/pre_selected_salmonella.txt") %>%
   select(gene_name, symbol)
 
 # Columns must be genes genes!!
-correlated_distance <- function(X, method=c("cosine", "pearson")){
+correlated_distance <- function(X, method_d=c("cosine", "pearson", "spearman")){
   
-  if(method=="cosine"){
+  if(method_d=="cosine"){
     X.t <- t(X) # Rows are genes
     sim <- X.t / sqrt(rowSums(X.t * X.t))
     sim <- sim %*% t(sim)
     
-  }else if(method=="pearson"){
-    sim <- cor(X, method = "pearson")
+  }else{
+    sim <- cor(X, method = method_d)
   }
   
   dist.mat <- 1 - sim
   dist.obj <- as.dist(dist.mat)
   
   return(dist.obj)
+}
+
+correlated_distance_matrix <- function(X, method_d=c("cosine", "pearson", "spearman")){
+  
+  if(method_d=="cosine"){
+    X.t <- t(X) # Rows are genes
+    sim <- X.t / sqrt(rowSums(X.t * X.t))
+    sim <- sim %*% t(sim)
+    
+  }else{
+    sim <- cor(X, method = method_d)
+  }
+  
+  dist.mat <- 1 - sim
+  
+  return(dist.mat)
+}
+
+# Some info on distances
+species_distances <- function(species, mem.df, d){
+  spec_members <- mem.df %>% 
+    filter(organism == species) %>% 
+    select(gene_name)
+  
+  if(length(spec_members$gene_name) <= 1){
+    return(data.frame("gene_name"=spec_members$gene_name,
+                      "species_max_distance"=0))
+  }
+  
+  spec_dist <- d[spec_members$gene_name, spec_members$gene_name]
+  spec_max_distances <- apply(spec_dist, 1, max)
+  
+  s.df <- data.frame("gene_name"=names(spec_max_distances),
+                     "species_max_distance"=spec_max_distances)
+  
+  return(s.df)
+  
+}
+
+gene_distances <- function(members, dmat){
+  
+  if(length(members) <= 1){
+    return(data.frame("gene_name"=members,
+                      "cluster_max_distance"=0,
+                      "species_max_distance"=0))
+  }
+  
+  clus_dist <- dmat[members, members]
+  clus_max_distances <- apply(clus_dist, 1, max)
+  
+  o.df <- data.frame("gene_name"=names(clus_max_distances),
+                     "cluster_max_distance"=clus_max_distances,
+                     "organism"=sapply(names(clus_max_distances), function(x){tail(strsplit(x,"-")[[1]], n=1)}))
+  
+  sp.df <- bind_rows(lapply(unique(o.df$organism), species_distances, o.df, clus_dist))
+  
+  distance_info <- o.df %>% 
+    left_join(sp.df, by="gene_name") %>% 
+    select(-organism)
+  
+  return(distance_info)
 }
 
 
@@ -189,40 +276,100 @@ server <- function(input, output){
   ## CDS
   clust_obj_cds <- reactive({
     dist_obj <- correlated_distance(t(clustering_parameters_cds$df), 
-                                    method = clustering_parameters_cds$d_metric)
+                                    method_d = clustering_parameters_cds$d_metric)
     
     if(clustering_parameters_cds$c_method == "minmax"){
       cobj <- protoclust(dist_obj)
-    }else if(clustering_parameters_cds$c_method == "complete"){
-      cobj <- hclust(dist_obj, method = "complete")
+    }else{
+      cobj <- hclust(dist_obj, method = clustering_parameters_cds$c_method)
+    }
+    cobj
+  })
+
+  ## sRNA
+  clust_obj_srna <- reactive({
+    dist_obj <- correlated_distance(t(clustering_parameters_srna$df), 
+                                    method_d = clustering_parameters_srna$d_metric)
+    
+    if(clustering_parameters_srna$c_method == "minmax"){
+      cobj <- protoclust(dist_obj)
+    }else{
+      cobj <- hclust(dist_obj, method = clustering_parameters_srna$c_method)
     }
     cobj
   })
   
+  # Clustering Matrix
+  ## CDS
+  clust_dist_cds <- reactive({
+    correlated_distance_matrix(t(clustering_parameters_cds$df),
+                               method_d = clustering_parameters_cds$d_metric)
+  })
+  
   ## sRNA
-  clust_obj_srna <- reactive({
-    dist_obj <- correlated_distance(t(clustering_parameters_srna$df), 
-                                    method = clustering_parameters_srna$d_metric)
-    
-    if(clustering_parameters_srna$c_method == "minmax"){
-      cobj <- protoclust(dist_obj)
-    }else if(clustering_parameters_srna$c_method == "complete"){
-      cobj <- hclust(dist_obj, method = "complete")
-    }
-    cobj
+  clust_dist_srna <- reactive({
+    correlated_distance_matrix(t(clustering_parameters_srna$df),
+                               method_d = clustering_parameters_srna$d_metric)
+  })
+  
+  # Dendrogram Parameters
+  ## CDS
+  dendro_high_cds <- reactiveValues(
+    c_high = 0
+    )
+  
+  ##sRNA
+  dendro_high_srna <- reactiveValues(
+    c_high = 0
+  )
+  
+  # Update highlight
+  ## CDS
+  observeEvent(input$high_param_cds, {
+    dendro_high_cds$c_high <- input$high_cds
+  })
+  
+  ## sRNA
+  observeEvent(input$high_param_srna, {
+    dendro_high_srna$c_high <- input$high_srna
   })
   
   # Dendogram
   ## CDS
   output$dendo_cds <- renderPlot({
-    plot(clust_obj_cds(), labels=rep("", nrow(clustering_parameters_cds$df)))
-    rect.hclust(clust_obj_cds(), k = clustering_parameters_cds$k_clusters)
+    
+    if(clustering_parameters_cds$c_method == "minmax"){
+      cut <- protocut(clust_obj_cds(), k=clustering_parameters_cds$k_clusters)
+    }else{
+      cut <- list("cl"=cutree(clust_obj_cds(), k=clustering_parameters_cds$k_clusters))
+    }
+    
+    if(dendro_high_cds$c_high == 0){
+      plot(clust_obj_cds(), labels=rep("", nrow(clustering_parameters_cds$df)))
+      rect.hclust(clust_obj_cds(), k = clustering_parameters_cds$k_clusters) 
+    }else{
+      plot(clust_obj_cds(), 
+           labels=sapply(cut$cl, function(x){if(x==dendro_high_cds$c_high){return("*")}else{return("")}}))
+      rect.hclust(clust_obj_cds(), k = clustering_parameters_cds$k_clusters)
+    }
     })
   
   ## sRNA
   output$dendo_srna <- renderPlot({
-    plot(clust_obj_srna(), labels=rep("", nrow(clustering_parameters_srna$df)))
-    rect.hclust(clust_obj_srna(), k = clustering_parameters_srna$k_clusters)
+    if(clustering_parameters_srna$c_method == "minmax"){
+      cut <- protocut(clust_obj_srna(), k=clustering_parameters_srna$k_clusters)
+    }else{
+      cut <- list("cl"=cutree(clust_obj_srna(), k=clustering_parameters_srna$k_clusters))
+    }
+    
+    if(dendro_high_srna$c_high == 0){
+      plot(clust_obj_srna(), labels=cut$cl, cex=0.45)
+      rect.hclust(clust_obj_srna(), k = clustering_parameters_srna$k_clusters)
+    }else{
+      plot(clust_obj_srna(), 
+           labels=sapply(cut$cl, function(x){if(x==dendro_high_srna$c_high){return("*")}else{return("")}}))
+      rect.hclust(clust_obj_srna(), k = clustering_parameters_srna$k_clusters)
+    }
   })
   
   
@@ -231,7 +378,7 @@ server <- function(input, output){
   cluster_info_cds <- reactive({
     if(clustering_parameters_cds$c_method == "minmax"){
       cut <- protocut(clust_obj_cds(), k=clustering_parameters_cds$k_clusters)
-    }else if(clustering_parameters_cds$c_method == "complete"){
+    }else{
       cut <- list("cl"=cutree(clust_obj_cds(), k=clustering_parameters_cds$k_clusters))
     }
     
@@ -260,7 +407,7 @@ server <- function(input, output){
   cluster_info_srna <- reactive({
     if(clustering_parameters_srna$c_method == "minmax"){
       cut <- protocut(clust_obj_srna(), k=clustering_parameters_srna$k_clusters)
-    }else if(clustering_parameters_srna$c_method == "complete"){
+    }else{
       cut <- list("cl"=cutree(clust_obj_srna(), k=clustering_parameters_srna$k_clusters))
     }
     
@@ -477,10 +624,18 @@ server <- function(input, output){
     f.df <- cluster_info_cds() %>% 
       filter(cluster == focus_parameters_cds$c_focus)
     
+    cluster_members <- f.df %>% 
+      select(gene_name) %>% 
+      unique()
+    
+    representative_info <- gene_distances(cluster_members$gene_name,
+                                          clust_dist_cds())
     
     g_feat <- genomic_features %>% 
-      filter(gene_name %in% f.df$gene_name) %>% 
-      mutate("Pre-selected"=if_else(gene_name %in% sussane.df$gene_name, "Pre-selected", ""))
+      filter(gene_name %in% cluster_members$gene_name) %>% 
+      mutate("Pre-selected"=if_else(gene_name %in% sussane.df$gene_name, "Pre-selected", "")) %>% 
+      left_join(representative_info, by="gene_name") %>% 
+      left_join(cds_avgExp, by="gene_name")
     
     if(clustering_parameters_srna$c_method == "minmax"){
       gprototypes <- f.df %>% filter(prototype=="Prototype")
@@ -498,10 +653,19 @@ server <- function(input, output){
     f.df <- cluster_info_srna() %>% 
       filter(cluster == focus_parameters_srna$c_focus)
     
+    cluster_members <- f.df %>% 
+      select(gene_name) %>% 
+      unique()
+    
+    representative_info <- gene_distances(cluster_members$gene_name,
+                                          clust_dist_srna())
+    
     
     g_feat <- genomic_features %>% 
       filter(gene_name %in% f.df$gene_name) %>% 
-      mutate("Pre-selected"=if_else(gene_name %in% sussane.df$gene_name, "Pre-selected", ""))
+      mutate("Pre-selected"=if_else(gene_name %in% sussane.df$gene_name, "Pre-selected", "")) %>% 
+      left_join(representative_info, by="gene_name") %>% 
+      left_join(srna_avgExp, by="gene_name")
     
     if(clustering_parameters_srna$c_method == "minmax"){
       gprototypes <- f.df %>% filter(prototype=="Prototype")
@@ -509,6 +673,8 @@ server <- function(input, output){
       g_feat <- g_feat %>% 
         mutate("Prototype" = if_else(gene_name %in% gprototypes$gene_name, "Prototype", ""))
     }
+    
+    g_feat
   })
   
 }
